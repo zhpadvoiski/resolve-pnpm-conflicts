6f34ff42b746df929d58cdb0ef379928811cef83
yauheni padvoiski <zh.padvoiski@gmail.com>
2024-12-03 10:41:41 -0600

    remane

--- src/merge-package-json.js
+++ src/merge-package-json.js
@@ -2,8 +2,8 @@ const mergePackageJson = require("merge-package.json");
 import fs from "fs";
 
 const localFilename = "package.json";
-const baseFilename = "package.json.base";
-const theirsFilename = "package.json.theirs";
+const baseFilename = "package.base.json";
+const theirsFilename = "package.theirs.json";
 
 const localJson = fs.readFileSync(localFilename, "utf-8");
 const baseJson = fs.readFileSync(baseFilename, "utf-8");
